758074f6f61487d3d1976c3c0c77f16846d1f0ed
dmck <dmck@users.noreply.github.com>
2016-02-27 12:14:12 -0500

    Switch to chrome.storage for fun and to fix the np.reddit.com bug

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /*
- *  Original work Copyright 2010 Hrishikesh Bakshi
  *  Modified work Copyright 2016 Daniel McKnight
+ *  Original work Copyright 2010 Hrishikesh Bakshi
  *  
- *  Reddit-Hide-Sidebar
+ *  Responsive-Reddit-Sidebar
  *  Responsively or manually hide Reddit's sidebar.
  */
 
@@ -21,20 +21,20 @@ function hideSidebar() {
     /* 1) change text, 2) hide, 3) set status */
     $("a#hslink").text(showSidebarText);
     $('div.side').hide();
-    localStorage['sidebarStatus'] = 'hide';
+    chrome.storage.local.set({'sidebarStatus': 'hide'});
 }
 function showSidebar() {
     $("a#hslink").text(hideSidebarText);
     $('div.side').show();
-    localStorage['sidebarStatus'] = 'show';
+    chrome.storage.local.set({'sidebarStatus': 'show'});
 }
 function lockSidebar() {
     $("a#lockLink").text(unlockSidebarText);
-    localStorage["lockStatus"] = "locked";
+    chrome.storage.local.set({'lockStatus': 'locked'});
 }
 function unlockSidebar() {
     $("a#lockLink").text(lockSidebarText);
-    localStorage["lockStatus"] = "unlocked";
+    chrome.storage.local.set({'lockStatus': 'unlocked'});
 }
 function respond() {
     if ($( window ).width() < breakpoint) {
@@ -44,37 +44,80 @@ function respond() {
     }
 }
 
+/*
+* We do two (3) main tasks when the page is loaded:
+* 1) Read the localStorage to resume state, if any
+* 2) Initialize the "on-page / in-tab storage"
+*    (a.k.a. the toggle links for 'show/hide' etc.)
+* 3) Hide the listing-chooser when away from Front-Page
+*/
 $( document ).ready(function() {
+	// Setup the area wher the toggles will go.
     $("div#header-bottom-right").append(show);
     
-    $('#lockLink').text(lockSidebarText);
-    if (localStorage["lockStatus"] == "unlocked") {
-        respond();
-    } else {
-        if (localStorage["sidebarStatus"] == "hide") {
-            hideSidebar();
-            $('#hslink').text(showSidebarText);
-        } else {
-            showSidebar();
-            $('#hslink').text(hideSidebarText);
-        }
+    chrome.storage.local.get('lockStatus', function(lockStatus) {
+    	console.log("On ready lockStatus:" + lockStatus['lockStatus']);
+	    // First check localStorage to resume last state
+	    if (lockStatus['lockStatus'] == 'locked') {
+	    	// If it's locked the give user link to unlock.   	
+	    	$('#lockLink').text(unlockSidebarText);
 
-    }
+	    	/*
+	    	* If the sidebar is locked, then we know this
+	    	* is not the first time this extension has 
+	    	* been loaded, and we will apply the previous
+	    	* status of the sidebar:
+	    	*/
+	    	chrome.storage.local.get('sidebarStatus', function(sidebarStatus) {
+		        if (sidebarStatus['sidebarStatus'] == "hide") {
+		        	// If locked in 'hide' position, then hide:
+		            $('#hslink').text(showSidebarText);
+		            hideSidebar();
+		        } else {
+		        	// If locked in 'show' position, then show:
+		            $('#hslink').text(hideSidebarText);
+		            showSidebar();
+		        }
+	    	});
+	    } else {
+	    	/*
+	    	* If sidebar isn't locked then it may be the
+	    	* first load, or it may be unlocked. Either way
+	    	* let's be "responsive".
+	    	*/
+	    	$('#lockLink').text(lockSidebarText);
+	        respond();
+	    }
 
+	});
+
+
+    /*
+    * It's annoying when the multi-reddit chooser - or
+    * as Reddit calls it - the "listing-chooser" stays
+    * open even after you've clicked an listing/multi
+    * and have navigated away from the Front Page.
+    * For this reason, we detect whether the we're on
+    * the front page, and hide the listing-chooser if
+    * we are on a different page.
+    */
 	var parser = document.createElement('a');
 	parser.href = window.location.pathname;
-
-	//alert(parser.pathname);
-	if(parser.pathname != "/") {
+	if(parser.pathname != '/') {
 		$('body').addClass('listing-chooser-collapsed');
 	}
-
 });
 
+/*
+* If sidebar isn't locked, then the sidebar should 
+* respond when window is resized.
+*/
 $( window ).resize(function() {
-    if (localStorage["lockStatus"] == "unlocked") {
-        respond();
-    }
+	chrome.storage.local.get('lockStatus', function(lockStatus) {
+	    if (lockStatus['lockStatus'] == 'unlocked') {
+	        respond();
+    	}
+	});
 });
 
 /* -- Show/Hide Sidebar -- */
